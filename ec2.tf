b475001d675c995a9a2f4b7eb822cff282372010
sobi3ch <piotr.sobieszczanski@gmail.com>
2020-03-28 20:33:45 +0100

    Adding auto generating ssh key pair with private key store inside terraform directory

--- ec2.tf
+++ ec2.tf
@@ -14,20 +14,43 @@
 #   owners = ["099720109477"] # Canonical
 # }
 
+# Generate SSH key pair
+resource "tls_private_key" "generated_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# EC2 key pair
+resource "aws_key_pair" "web" {
+  key_name   = "ssh-${var.name}-${local.client}"
+  public_key = tls_private_key.generated_key.public_key_openssh
+  tags = merge(local.common_tags, {
+    "Name" = var.name
+    "Environment" = var.env
+  })
+}
+
 # EC2
 resource "aws_instance" "web" {
   # ami           = data.aws_ami.ubuntu.id
   ami           = "ami-083b53142137770ad"
   instance_type = "t3.micro"
   subnet_id = aws_subnet.website-public[0].id
-  key_name = "aws.stockholm.xps13"
+  key_name  = aws_key_pair.web.key_name
   vpc_security_group_ids = [
     aws_security_group.webserver.id,
     aws_security_group.ssh.id
   ]
 
   tags = merge(local.common_tags, {
-    "Name" = "${var.name}"
+    "Name" = var.name
     "Environment" = var.env
   })
+}
+
+resource "local_file" "ssh_private_key" {
+  sensitive_content = tls_private_key.generated_key.private_key_pem
+  filename = "ssh-${var.name}-${local.client}.pem"
+  file_permission = "0600"
+  depends_on = [ tls_private_key.generated_key ]
 }
