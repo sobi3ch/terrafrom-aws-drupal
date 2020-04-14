4c4ba99e2cb5b28cad7d956f262b668e3b953190
sobi3ch <piotr.sobieszczanski@gmail.com>
2020-04-14 20:13:38 +0200

    Full ALB added

--- load_balancer.tf
+++ load_balancer.tf
@@ -4,3 +4,28 @@ resource "aws_lb_target_group" "drupal" {
   protocol = "HTTP"
   port     = 80
 }
+
+resource "aws_lb" "drupal" {
+  name               = "drupal-terrafrom-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [ module.networking.webserver_sg.id ]
+  subnets            = module.networking.subnets_public
+
+  # enable_deletion_protection = true
+
+  tags = {
+    Environment = "test"
+  }
+}
+
+resource "aws_lb_listener" "drupal" {
+  load_balancer_arn = aws_lb.drupal.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.drupal.arn
+  }
+}
